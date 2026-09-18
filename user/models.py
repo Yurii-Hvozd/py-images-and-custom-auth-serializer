38485a5acf756c2d230d4af212e3d6be2a943f12
Yurii <yura.gvozd.2002@gmail.com>
2026-09-18 22:59:27 +0300

    'Solution4'

--- user/models.py
+++ user/models.py
@@ -15,7 +15,7 @@ class UserManager(DjangoUserManager):
         if not email:
             raise ValueError("The given email must be set")
 
-        email = self.normalize_email(email)
+        extra_fields.setdefault("username", email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
