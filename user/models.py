e63f8e3e457f66625e2047c5a0f52e870e92a28c
Yurii <yura.gvozd.2002@gmail.com>
2026-09-18 23:03:53 +0300

    'Solution5'

--- user/models.py
+++ user/models.py
@@ -42,7 +42,6 @@ class UserManager(DjangoUserManager):
 
 
 class User(AbstractUser):
-    username = None
     email = models.EmailField(_("email address"), unique=True)
 
     USERNAME_FIELD = "email"
